71672588026969ae5ad0a73010750a2d95dc5367
prathmeshgodbole <godbole611@gmail.com>
2021-08-10 12:44:20 +0530

    Todo-List

---
+++ .history/src/components/CourseGoals/CourseInput/CourseInput_20210810114454.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Button from "../../UI/Button/Button";
+import styles from './CourseInput.module.css';
+
+
+
+// & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+// }
+
+// ${'' /* 
+// &.invalid input {
+//     border-color: red;
+//     background: #ffd7d7;
+// }
+
+// &.invalid label {
+//     border-color: red;
+// } */}
+
+// `;
+
+const CourseInput = (props) => {
+    const [enteredValue, setEnteredValue] = useState("");
+    const [isValid, setIsValid] = useState(true);
+
+    const goalInputChangeHandler = (event) => {
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
+        setEnteredValue(event.target.value);
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+        props.onAddGoal(enteredValue);
+    };
+
+    return ( <
+        form onSubmit = { formSubmitHandler } >
+        <
+        div className = { `${styles['form-control']} ${!isValid && styles.invalid}` } >
+        <
+        label > Course Goal < /label> <
+        input type = "text"
+        onChange = { goalInputChangeHandler }
+        /> <
+        /div> <
+        Button type = "submit" > Add Goal < /Button> <
+        /form>
+    );
+};
+
+export default CourseInput;
